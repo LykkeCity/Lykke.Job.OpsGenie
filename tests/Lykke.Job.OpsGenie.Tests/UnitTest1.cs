ea17a21199c82fbf373ff7a2821e536e3f73a4c3
avolkov <avolkov.dev@gmail.com>
2018-09-06 12:59:08 +0300

    disable sample unit test

--- tests/Lykke.Job.OpsGenie.Tests/UnitTest1.cs
+++ tests/Lykke.Job.OpsGenie.Tests/UnitTest1.cs
@@ -1,41 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Lykke.Job.OpsGenie.Core.Services.OpsGenieApi;
-using Lykke.Job.OpsGenie.Services.OpsGenieApi;
-using Xunit;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Threading.Tasks;
+//using Lykke.Job.OpsGenie.Core.Services.OpsGenieApi;
+//using Lykke.Job.OpsGenie.Services.OpsGenieApi;
+//using Xunit;
 
-namespace Lykke.Job.OpsGenie.Tests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public async Task Test1()
-        {
-            var adapter = new OpsGenieApiAdapter(new OpsGenieApiAdapterSettings { ApiKey = "" });
+//namespace Lykke.Job.OpsGenie.Tests
+//{
+//    public class UnitTest1
+//    {
+//        [Fact]
+//        public async Task Test1()
+//        {
+//            var adapter = new OpsGenieApiAdapter(new OpsGenieApiAdapterSettings { ApiKey = "" });
 
-            try
-            {
-                var notif = await adapter.CreateAlert(new Alert
-                {
-                    Message = "message",
-                    Details = new Dictionary<string, object>() { { "key", "value" } },
-                    Actions = new List<string>() { "action1" },
-                    User = "user",
-                    Tags = new List<string>() { "tag" },
-                    Description = "desction",
-                    Alias = Guid.NewGuid().ToString(),
-                    Entity = "Entio",
-                    Source = "Source"
-                });
+//            try
+//            {
+//                var notif = await adapter.CreateAlert(new Alert
+//                {
+//                    Message = "message",
+//                    Details = new Dictionary<string, object>() { { "key", "value" } },
+//                    Actions = new List<string>() { "action1" },
+//                    User = "user",
+//                    Tags = new List<string>() { "tag" },
+//                    Description = "desction",
+//                    Alias = Guid.NewGuid().ToString(),
+//                    Entity = "Entio",
+//                    Source = "Source"
+//                });
 
-                var t2 = notif;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
-        }
-    }
-}
+//                var t2 = notif;
+//            }
+//            catch (Exception e)
+//            {
+//                Console.WriteLine(e);
+//                throw;
+//            }
+//        }
+//    }
+//}
